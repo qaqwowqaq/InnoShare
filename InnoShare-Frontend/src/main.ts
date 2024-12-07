9a570193d4a8a58df74c8f312b32cc37c90b93b1
PLUTOFX <1447415885@qq.com>
2024-12-07 10:11:52 +0800

    个人门户管理-解决element plus样式不显示问题，新增获取专属邀请码功能

--- InnoShare-Frontend/src/main.ts
+++ InnoShare-Frontend/src/main.ts
@@ -7,7 +7,6 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import './styles/index.css'
 import 'element-plus'
-import { createPinia } from 'pinia'
 import { mainStore } from './store/modules/pageStyleStore.ts'; // 引入 mainStore
 
 const app = createApp(App);
--- InnoShare-Frontend/src/router/index.ts
+++ InnoShare-Frontend/src/router/index.ts
@@ -1,17 +1,52 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 import PaperDetail from '@/views/PaperDetail.vue';
 import AchiManage from '@/views/AchiManage.vue';
 import UserDashboard from "@/views/UserDashboard.vue";
+import SearchResults from '@/views/SearchResults.vue'
 const routes: Array<RouteRecordRaw> = [
+
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
+    redirect:'home'
 
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home,
+    children: [
+      {
+        path: '', // 默认子路由
+        name:'default',
+        redirect:"/home/recommend",
+      },
+      {
+        path: '/home/recommend',
+        component: () => import("../components/modules/homeComponent/recommendList.vue")
+      },
+      {
+        path: '/home/hot',
+        component: () => import("../components/modules/homeComponent/hotList.vue")
+      },
+      {
+        path: '/home/new',
+        component: () => import("../components/modules/homeComponent/newList.vue")
+      },
+    ]
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/search',
+    name: 'SearchResults',
+    component: SearchResults,
+  },
   {
     path: '/login',
     name: 'Login',
@@ -21,6 +56,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/register',
     name: 'Register',
     component: Register
+  },
+  {
+    path: '/userDashboard/:userId',
+    name: 'UserDashboard',
+    component: UserDashboard
   },
   {
     path: '/paper/:id',
@@ -40,7 +80,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 });
 
